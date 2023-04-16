5ec6981c578bd50aab3c17f8633bc26a34566998
Anthony <anthony.ngo@viget.com>
2023-04-16 12:43:52 -0700

    create pages and update app/config/routes.rb

--- rails-pages/config/routes.rb
+++ rails-pages/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get '/services', to: 'services#index'
+  get '/about', to: 'about#index'
+  # get 'services/index'
+  # get 'about/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
