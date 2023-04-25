4ea688cc81a2b8ed4ce4d126d096815bd9e376e5
Anthony <anthony.ngo@viget.com>
2023-04-25 00:39:04 -0700

    update authentication

--- rails-devise/config/routes.rb
+++ rails-devise/config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
